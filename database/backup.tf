863a54db062d754fd987065e9fc15fc68239cac3
Oluty-1 <tayoefunshile@gmail.com>
2024-10-24 00:25:10 +0100

    configured backup

--- database/backup.tf
+++ database/backup.tf
@@ -1,36 +1,42 @@
-resource "aws_backup_plan" "aurora_backup" {
-  name = "aurora-cross-region-backup"
-
-  rule {
-    rule_name         = "daily-backup"
-    target_vault_name = aws_backup_vault.aurora_vault.name
-    schedule          = "cron(0 5 * * ? *)"
-    
-    lifecycle {
-      delete_after = 14
-    }
-
-    copy_action {
-      destination_vault_arn = aws_backup_vault.aurora_vault_dr.arn
-    }
-  }
-}
-
 resource "aws_backup_vault" "aurora_vault" {
+  provider = aws.primary
   name     = "aurora-backup-vault"
 }
 
 resource "aws_backup_vault" "aurora_vault_dr" {
-  provider = aws.secondary
+  provider = aws.backup
   name     = "aurora-backup-vault-dr"
 }
 
+resource "aws_backup_plan" "aurora_backup" {
+  provider = aws.primary
+  name     = "aurora-cross-region-backup"
+
+  rule {
+    rule_name         = "daily-backup"
+    target_vault_name = aws_backup_vault.aurora_vault.name
+    schedule          = "cron(0 5 * * ? *)"  # Daily at 5 AM UTC
+    
+    lifecycle {
+      delete_after = 14 
+    }
+
+    copy_action {
+      destination_vault_arn = aws_backup_vault.aurora_vault_dr.arn
+      lifecycle {
+        delete_after = 14
+      }
+    }
+  }
+}
+
 resource "aws_backup_selection" "aurora_backup" {
-  iam_role_arn = aws_iam_role.backup_role.arn
-  name         = "aurora-backup-selection"
-  plan_id      = aws_backup_plan.aurora_backup.id
+  provider      = aws.primary
+  name          = "aurora-backup-selection"
+  iam_role_arn  = aws_iam_role.backup_role.arn
+  plan_id       = aws_backup_plan.aurora_backup.id
 
   resources = [
-    aws_rds_cluster.aurora.arn
+    aws_rds_cluster.main.arn
   ]
 }
